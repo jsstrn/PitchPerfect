f107be7fe0d38ad9221d9d9939ebe80958fccf03
jsstrn <jessternrays@gmail.com>
2015-03-05 16:10:24 +0800

    added button images, outlets, and actions.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        // toggle display text "recording..."
+        if (recordingInProgress.hidden) {
+            recordingInProgress.hidden = false
+        } else {
+            recordingInProgress.hidden = true
+        }
+        // TODO: record user's voice 
+        println("You clicked on the record button")
+    }
 
 }
 
